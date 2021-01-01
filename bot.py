d54f6af0ad23a60cf77ba212934a4e926c1ca1c4
Evil-Ezio <67472819+Evil-Ezio@users.noreply.github.com>
2021-01-01 15:01:36 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,30 @@
+from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+import time
+import re
+import os.path
+from os import path
+from datetime import datetime
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.common.exceptions import NoSuchElementException        
 
+opt = Options()
+opt.add_argument("--disable-infobars")
+opt.add_argument("start-maximized")
+opt.add_argument("--disable-extensions")
+opt.add_argument("--start-maximized")
+
+opt.add_experimental_option("prefs", { \
+    "profile.default_content_setting_values.media_stream_mic": 1, 
+    "profile.default_content_setting_values.media_stream_camera": 1,
+    "profile.default_content_setting_values.geolocation": 1, 
+    "profile.default_content_setting_values.notifications": 1 
+  })
+
+driver = None
+URL = "https://teams.microsoft.com"
+
+CREDS = {'email' : 'example@gmail.com','passwd':'password'}
